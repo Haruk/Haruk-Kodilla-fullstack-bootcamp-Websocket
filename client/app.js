48caf548b2a5fb1f22f4ba9000d2cdb3362fdd36
Aleksandra <haruka.lex@gmail.com>
2023-03-09 10:51:05 +0100

    Add websocket

--- client/app.js
+++ client/app.js
@@ -1,63 +1,58 @@
-const loginForm = document.querySelector(`#welcome-form`);
-const messagesSection = document.querySelector(`#messages-section`);
-const messagesList = document.querySelector(`#messages-list`);
-const addMessageForm = document.querySelector(`#add-messages-form`);
-const userNameInput = document.querySelector(`#username`);
-const messageContentInput = document.querySelector(`#message-content`);
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
 
-let userName = ``;
+const socket = io();
 
+let userName = '';
 
 const login = (e) => {
-    e.preventDefault();
-
-    const isUserNameInputEmpty = userNameInput.value === `` ? true : false;
-
-    if (isUserNameInputEmpty) {
-        alert(`Puste pole`)
-        return 0
-    }
-
+  e.preventDefault();
+  if (!userNameInput.value) {
+    alert('User Name must not be empty');
+  } else {
     userName = userNameInput.value;
-
-    loginForm.classList.remove(`show`);
-    messagesSection.classList.add(`show`);
-
-}
+    socket.emit('userName', userName);
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  }
+};
 
 const addMessage = (author, content) => {
-    const message = document.createElement('li');
-    message.classList.add('message');
-    message.classList.add('message--received');
-    if (author === userName) message.classList.add('message--self');
-    message.innerHTML = `
-      <h3 class="message__author">${userName === author ? 'You' : author}</h3>
-      <div class="message__content">
-        ${content}
-      </div>
-    `;
-    messagesList.appendChild(message);
-}
-
+  const message = document.createElement('li');
+  message.classList.add('message', 'message--received');
+  if (author === userName) {
+    message.classList.add('message--self');
+  }
+  if (author === 'Chat Bot') {
+    message.classList.add('message--bot');
+  }
+  message.innerHTML = ` <h3 class="message__author">${
+    author === userName ? 'You' : author
+  }</h3>
+    <div class="message__content">${content}</div>
+  </li>`;
+  messagesList.appendChild(message);
+};
 
 const sendMessage = (e) => {
-    e.preventDefault()
-
-    const isMessageInputEmpty = messageContentInput.value === `` ? true : false;
-
-    if (isMessageInputEmpty) {
-        alert(`Puste pole`)
-        return 0
-    }
-
+  e.preventDefault();
+  if (!messageContentInput.value) {
+    alert('You must write the message');
+  } else {
     addMessage(userName, messageContentInput.value);
-    messageContentInput.value = ``;
+    socket.emit('message', {
+      author: userName,
+      content: messageContentInput.value,
+    });
+    messageContentInput.value = '';
+  }
+};
 
-}
+socket.on('message', ({ author, content }) => addMessage(author, content));
 
-
-loginForm.addEventListener(`submit`, login);
-addMessageForm.addEventListener(`submit`, sendMessage)
-
-console.log(`witam`)
-console.log(loginForm)
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
